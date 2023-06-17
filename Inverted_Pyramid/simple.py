b2eba06c0f6bcee6e0acc460d690aa2dd61af57b
Rohit Chandrakant Gore <rohitchandrakantgore@gmail.com>
2023-06-17 15:23:29 +0530

    Patterns to Notebooks

--- Inverted_Pyramid/simple.py
+++
@@ -1,3 +0,0 @@
-height = int(input("Enter Height: "))
-for i in range(height,0,-1):
-    print(' '*(height-i)+'* '*i )
--- Pyramid/star.py
+++
@@ -1,4 +0,0 @@
-height = int(input("Enter Height: "))
-
-for i in range(height):
-    print(" "*(height-i)+'* '*(i+1))
